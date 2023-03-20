91cda295fff5a4f8eea015615e2547df3d70db39
Alexander Rattray <alexander_rattray@aimltd.uk>
2023-03-20 14:20:07 +0000

    created route and new view for ng-view

--- Module4Code/Code/Begin/WorkingWithRoutes/app/app.js
+++ Module4Code/Code/Begin/WorkingWithRoutes/app/app.js
@@ -1,5 +1,12 @@
-(function() {
-    
-    var app = angular.module('customersApp', ['ngRoute']);
-    
-}());
+(function () {
+  var app = angular.module("customersApp", ["ngRoute"]);
+
+  app.config(function ($routeProvider) {
+    $routeProvider
+      .when("/", {
+        controller: "CustomersController",
+        templateUrl: "app/views/customers.html",
+      })
+      .otherwise({ redirectTo: "/" });
+  });
+})();
