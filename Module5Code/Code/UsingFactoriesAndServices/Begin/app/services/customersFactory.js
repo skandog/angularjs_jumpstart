880b74aba29a1cadd72c386c0ff075bd519c8424
Alexander Rattray <alexander_rattray@aimltd.uk>
2023-03-20 16:44:59 +0000

    updated orders controller to utilise factory, and updated view to personalise to each customer

--- Module5Code/Code/UsingFactoriesAndServices/Begin/app/services/customersFactory.js
+++ Module5Code/Code/UsingFactoriesAndServices/Begin/app/services/customersFactory.js
@@ -71,10 +71,20 @@
 
     return {
       getCustomers: getCustomers,
+      getCustomer: getCustomer,
     };
 
     function getCustomers() {
       return customers;
+    }
+
+    function getCustomer(customerId) {
+      for (var i = 0, len = customers.length; i < len; i++) {
+        if (customers[i].id === parseInt(customerId)) {
+          return customers[i];
+        }
+      }
+      return {};
     }
   }
 })();
